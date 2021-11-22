1fd4b59a0ceb7c6da6fe18d8d3243b43fbdf25d0
zougit <vettesegiovanni@gmail.com>
2021-11-23 00:29:47 +0100

    merge

---
+++ headers/Player.h
@@ -0,0 +1,27 @@
+//
+// Created by vette on 29/10/2021.
+//
+
+#ifndef MALLOCWORLD_PLAYER_H
+#define MALLOCWORLD_PLAYER_H
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct Player {
+    int currentHp;
+    int maxHp;
+    int exp;
+    int maxExp;
+    int level;
+    int value;
+    int x;
+    int y;
+    int orientation;// ANCHOR 
+                    // 1 haut
+                    // 2 gauche
+                    // 3 droite
+                    // 4 bas
+}Player;
+
+
+#endif //MALLOCWORLD_PLAYER_H
